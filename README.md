6e997fdc2f4eefb17a7dd122ceb67785870ea304
Maciej Sypien <maciejsypien@gmail.com>
2022-09-25 14:45:37 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Port scanner
+
+Dead simple fast port scanner.
+
+## Usage
+
+```bash
+port-scanner "localhost"
+```
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sort"
+)
+
+func worker(ports, results chan int) {
+	for p := range ports {
+		address := fmt.Sprintf("127.0.0.1:%d", p)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		if err := conn.Close(); err != nil {
+			fmt.Println("connection could not be closed", err)
+		}
+		results <- p
+	}
+}
+
+func main() {
+	// given host
+	host := os.Args[0]
+	if host == "" {
+		log.Fatal("no host provided")
+	}
+
+	ports := make(chan int, 100)
+	results := make(chan int)
+	var openports []int
+	portLimit := 65535
+
+	// spawning workers
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	// asigning tasks to ports
+	go func() {
+		for i := 1; i <= portLimit; i++ {
+			ports <- i
+		}
+	}()
+
+	// reading results
+	for i := 0; i < portLimit; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+	close(ports)
+	close(results)
+
+	// displaying reslts
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("port %d is open\n", port)
+	}
+	fmt.Printf("%d total ports open\n", len(openports))
+}
